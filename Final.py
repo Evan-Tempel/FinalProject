8e9cb5aba59b18be072992f5a70c512308731556
Evan <evantempel@gmail.com>
2017-04-02 14:50:01 -0400

    Added comments and started to work on password checks.

--- Final.py
+++ Final.py
@@ -1,4 +1,4 @@
-import getpass, os.path
+import getpass, os.path, sys
 
 
 version = '0.1'
@@ -9,36 +9,58 @@ ask_usr = int(input('Are you a return user(1)? or a new user(2)? '))
 
 fileCheck = os.path.isfile('test.txt')
 
-if ask_usr == 1:
+if ask_usr == 1: # Returning User.
 
     print('Welcome back returning user.')
     usrName = input('Username: ')
     passwd = getpass.getpass("Password: ") # Only shows up when running in terminal. Not in PyCharm.
 
+    # Add Code here to make sure the Username and Password is valid
+
     addorReview = int(input('Would you like to view usernames and passwords(1) or add new ones(2)'))
 
-    if addorReview == 1:
-        if fileCheck == True:
+    if addorReview == 1: # User is reviewing usernames and passwords
+
+        if fileCheck == True: # If user has previously saved a file, this is where it should be.
+
             usr_passFile = open('test.txt', 'r')
             print(usr_passFile.read())
             cont = int(input('Would you like to add to this(1) or exit the program(2)?'))
-        elif fileCheck == False:
+
+        elif fileCheck == False: # Users first time signing in, should not have any information
+
             print('You do not have anything related to this account.')
-        else:
+
+        else: # We should never be here, this is bad. Break the code to ensure integrity.
+
             print('Something went very wrong. Panic.')
-    elif addorReview == 2:
-        if fileCheck == True:
+            sys.out()
+
+    elif addorReview == 2: # User wants to add a username/password set
+
+        if fileCheck == True: # User's file has been created already. Append instead of write.
+
             usr_passFile = open('test.txt', 'a')
+            usr = str(input('What is the username: '))
+            password = getpass.getpass('Password for that username: ')
+            password2 = getpass.getpass('Re-enter the password: ')
+
+            if password == password2:
+
         elif fileCheck == False:
+
             usr_passFile = open('test.txt', 'w')
+
         else:
+
             print('Something went wrong, please panic!')
         print('We made it to two')
 
     else:
+
         print('One does not comprehend.')
 
-elif ask_usr == 2:
+elif ask_usr == 2: # New user, registration control
 
     print('We made it to 2!')
 
