f506ae458b5106d5c749401c7841c97c905a3f00
Evan <evantempel@gmail.com>
2017-04-16 15:34:26 -0400

    Added a new .py for majority of functions.
    Basically just create functions for everything!

--- functionFinal.py
+++ functionFinal.py
@@ -3,27 +3,27 @@ import bcrypt
 import pymysql
 
 
-def passCheck(string1, string2):
-    if (string1 == string2) == True:
-        return True
-    if (string1 == string2) == False:
-        return False
-
 def login():
 
     ask = input('What is your username?')
     askPass = getpass.getpass('Please enter your password: ')
     salt = bcrypt.gensalt()
     hash_PW = bcrypt.hashpw(askPass, salt)
-
     # Connect to DB to select username and to select password hash
-
     hashEval = (hash_PW == bcrypt.hashpw(askPass, hash_PW))
     return hashEval
 
 def createDBUser(username, usrPassword):
     conn = pymysql.connect(host='putIPhere', unix_socket='/tmp/mysql.sock', user='root', passwd=None, db='mysql')
 
-
-
+    # Create the user inside of the database.
     print('Account creation successful!')
+
+def createUsr():
+
+    print('We made it to 2!')
+    print('Welcome new user, please follow this wizard to create a new account!')
+    newUsr = str(input('Please enter a Username: '))
+    newPass = getpass.getpass('Please enter your password: ')
+    newPassCheck = getpass.getpass('Please reenter your password: ')
+
