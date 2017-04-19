031877e9eabe9cc3cb32e15800a261f733027bb7
evan <evantempel@gmail.com>
2017-04-19 14:59:20 -0400

    V0.3

--- functionFinal.py
+++ functionFinal.py
@@ -12,20 +12,37 @@ def login():
     conn = pymysql.connect(host='putIPhere', unix_socket='/tmp/mysql.sock', user=ask, passwd=hash_PW, db='PasswordManager')
 
     cur = conn.cursor()
-    view = input('Would you like to (1) view your list or (2) add to it or (3) exit')
-    li = []
-    if view == '1':
-        print('We made it to 1')
-        cur.execute("SELECT * FROM users")
-    elif view == '2':
-        print('We made it to two')
-    elif view == '3':
-        print('We made it to three')
-    else:
-        print('We\'ve ,ade a grave mistake')
+    check = True
+
+    while check == True:
 
 
-def createDBUser(username, usrPassword):
+        view = input('Would you like to (1) view your list or (2) add to it or (3) exit')
+
+        if view == '1':
+            print('We made it to 1')
+            cur.execute("SELECT * FROM users")
+        elif view == '2':
+            print('We made it to two')
+
+            accnt = str(input('Please enter the username you want to add: '))
+            accntPasswd = getpass.getpass('Please enter the password to that account: ')
+
+            cur.execute('INSERT INTO users ( username, passwd) VALUES /s,/s', (accnt, accntPasswd))
+
+        elif view == '3':
+            print('We made it to three')
+            check = False
+        else:
+            print('We\'ve ,ade a grave mistake')
+            check = False
+
+
+def createDBUser():
+
+    usrName = str(input('Please enter the desired username: '))
+    usrPasswd = getpass.getpass('Please enter the desired password: ')
+
     conn = pymysql.connect(host='putIPhere', unix_socket='/tmp/mysql.sock', user='root', passwd=None, db='mysql')
 
     # Create the user inside of the database.
