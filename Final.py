93e9c916cb822e15cd555d105315bb96b5987998
evan <evantempel@gmail.com>
2017-04-25 16:21:38 -0400

    Worked on DB

--- Final.py
+++ Final.py
@@ -1,13 +1,12 @@
 import getpass, os.path, sys, functionFinal, bcrypt
+import functionFinal
 
 
-version = '0.1'
+version = '1.0'
 
 print('Welcome to PyPassword Manager Version: ' + version)
 
-ask_usr = int(input('Are you a return user(1)? or a new user(2)? '))
-
-fileCheck = os.path.isfile('test.txt')
+ask_usr = str(input('Are you a return user(1)? or a new user(2)? '))
 
 check = True
 
@@ -17,95 +16,29 @@ while check == True:
 
     if ask_usr == '1':
 
-        functionFinal.login()
+        username, password = functionFinal.login()
+        functionFinal.returningUsr(username, password)
+
+        ask = input('Would you like to exit?(y/n) ')
+
+        if ask == 'y':
+            check = False
+        elif ask == 'n':
+            check == True
+            ask_usr = '1'
+        else:
+            print('You broke me')
 
     elif ask_usr == '2':
 
         functionFinal.createDBUser()
 
+        ask = input('Would you like to exit?(y/n) ')
 
-
-
-
-
-
-
-
-
-
-
-
-
-""" 
-
-Potentially outdated. This is hopefully being moved to mainly functions.
-if ask_usr == 1: # Returning User.
-
-    print('Welcome back returning user.')
-    usrName = input('Username: ')
-    passwd = getpass.getpass("Password: ") # Only shows up when running in terminal. Not in PyCharm.
-
-    # Add Code here to make sure the Username and Password is valid
-
-    addorReview = int(input('Would you like to view usernames and passwords(1) or add new ones(2)'))
-
-    if addorReview == 1: # User is reviewing usernames and passwords
-
-        if fileCheck == True: # If user has previously saved a file, this is where it should be.
-
-            usr_passFile = open('test.txt', 'r')
-            print(usr_passFile.read())
-            cont = int(input('Would you like to add to this(1) or exit the program(2)?')) # Needs to send be sent to a while loop.
-
-        elif fileCheck == False: # Users first time signing in, should not have any information
-
-            print('You do not have anything related to this account.')
-
-        else: # We should never be here, this is bad. Break the code to ensure integrity.
-
-            print('Something went very wrong. Panic.')
-            sys.out()
-
-    elif addorReview == 2: # User wants to add a username/password set
-
-        if fileCheck == True: # User's file has been created already. Append instead of write.
-
-            usr_passFile = open('test.txt', 'a')
-            usr = str(input('What is the username: '))
-            password = getpass.getpass('Password for that username: ')
-            password2 = getpass.getpass('Re-enter the password: ')
-
-            if password == password2: # Password check.
-                usr_passFile.write('Username: ' + usr + 'Password: ' + password2)
-                usr_passFile.close()
-
-        elif fileCheck == False: # Users first time. Create a new file.
-
-            usr_passFile = open('test.txt', 'w')
-
+        if ask == 'y':
+            check = False
+        elif ask == 'n':
+            check == True
+            ask_usr = '1'
         else:
-
-            print('Something went wrong, please panic!')
-
-        print('We made it to two')
-
-    else:
-
-        print('One does not comprehend.')
-
-elif ask_usr == 2: # New user, registration control
-
-    print('We made it to 2!')
-    print('Welcome new user, please follow this wizard to create a new account!')
-    newUsr = str(input('Please enter a Username: '))
-    newPass = getpass.getpass('Please enter your password: ')
-    newPassCheck = getpass.getpass('Please reenter your password: ')
-
-    if newPass == newPassCheck:
-        functionFinal.createDBUser()
-
-
-else: # Broken, we should never be here.
-
-    print('We broke it! :D')
-"""
+            print('You broke me')
--- functionFinal.py
+++ functionFinal.py
@@ -1,45 +1,86 @@
-import getpass
-import bcrypt
-import pymysql
+import getpass, bcrypt, pymysql, random, string
 
+
+saltpass = bcrypt.gensalt()
 
 def login():
 
-    ask = input('What is your username?')
-    askPass = getpass.getpass('Please enter your password: ')
-    salt = bcrypt.gensalt()
-    hash_PW = bcrypt.hashpw(askPass, salt)
+    username = input('Please enter a username')
+    password = getpass.getpass('Please enter the password: ')
 
+    enpass = password.encode('utf-8')
+    hash_PW = bcrypt.hashpw(enpass, saltpass)
+    depass = hash_PW.decode('utf-8')
 
-    # Connect to DB to select username and to select password hash
-    conn = pymysql.connect(host='localhost', user='ask', passwd=hash_PW, db='test')
+    conn = pymysql.connect(host='192.168.1.9', user=username, passwd=depass, db=username)
 
-
-
-    hashEval = (hash_PW == bcrypt.hashpw(askPass, hash_PW))
-    return hashEval
+    return username,depass
 
 def createDBUser():
-    conn = pymysql.connect(host='localhost', user='root', passwd=getpass.getpass('Please enter password'), db='test')
+    conn = pymysql.connect(host='192.168.1.9', user='Linux_Admin_Evan', passwd=getpass.getpass('Please enter password: '))
 
-    user1 = input('PLease enter the username: ')
+    createUsr = input('Please enter username: ')
 
-    userPasswd = getpass.getpass('Please enter the password: ')
-    userPasswdcheck = getpass.getpass('Please confirm the password: ')
-    if userPasswd == userPasswdcheck:
-        cur = conn.cursor()
-        cur.execute('CREATE USER %s@localhost IDENTIFIED BY %s', (user1, userPasswd))
+    createPass = getpass.getpass('Please enter password: ')
 
-        cur.execute('CREATE TABLE %s (username VARCHAR(50) DEFAULT NULL, usrpassword CHAR(61))' % user1)
-        cur.execute('GRANT ALL PRIVILEGES ON user.%s TO %s@localhost' % (user1, user1))
+    enpass = createPass.encode('utf-8')
+    hash_PW = bcrypt.hashpw(enpass, saltpass)
+
+    depass = hash_PW.decode('utf-8')
+
+
+    cur = conn.cursor()
+    cur.execute('CREATE DATABASE %s' % createUsr)
+    cur.execute('USE %s' % createUsr)
+
+    cur.execute('CREATE USER %s@192.168.1.9 IDENTIFIED BY %s', (createUsr, depass))
+    cur.execute('CREATE TABLE %s (username VARCHAR(50) DEFAULT NULL, userpassword CHAR(76))' % createUsr)
+    cur.execute('GRANT ALL PRIVILEGES ON *.* TO %s@192.168.1.9', createUsr)
 
     print('Account creation successful!')
 
-def createUsr():
+    return createUsr, depass
 
-    print('We made it to 2!')
+
+def returningUsr(username, password):
+
+    conn = pymysql.connect(host='192.168.1.9', user=username, passwd=password, db=username)
+
     print('Welcome new user, please follow this wizard to create a new account!')
-    newUsr = str(input('Please enter a Username: '))
-    newPass = getpass.getpass('Please enter your password: ')
-    newPassCheck = getpass.getpass('Please reenter your password: ')
 
+    askPass = str(input('Would you like to (1) add your own password or (2) use our random string generation or (3) View accounts and passwords?'))
+
+    cur = conn.cursor()
+
+    if askPass == '1':
+
+        askAccnt = str(input('Please give an account name: '))
+
+        newPass = getpass.getpass('Please enter a password: ')
+        newPassCheck = getpass.getpass('Please re-enter the password: ')
+        if newPass == newPassCheck:
+            enpass = newPass.encode('utf-8')
+            hash_PW = bcrypt.hashpw(enpass, saltpass)
+
+            depass = hash_PW.decode('utf-8')
+
+            cur.execute('INSERT INTO %s VALUES ("%s", "%s")' % (username, askAccnt, depass))
+            conn.commit()
+
+    elif askPass == '2':
+        askAccnt = str(input('Please give an account name: '))
+
+        ranString = ''.join(
+            random.SystemRandom().choice(string.ascii_uppercase + string.digits + string.ascii_lowercase) for _ in
+            range(24))
+
+        enpass = ranString.encode('utf-8')
+        hash_PW = bcrypt.hashpw(enpass, saltpass)
+
+        depass = hash_PW.decode('utf-8')
+
+        cur.execute('INSERT INTO %s VALUES ("%s", "%s")' % (username, askAccnt, depass))
+        conn.commit()
+
+    elif askPass == '3':
+        cur.execute('SELECT * FROM %s')
